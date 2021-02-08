6dd27db0c984e3905fa0c174bc946c2a9cd79770
Aytaj Nurullazada <aytacig@code.edu.az>
2021-02-09 02:38:25 +0400

    course details page created

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,17 +1,53 @@
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
-        },
-        600:{
-            items:2
-        },
-        1000:{
-            items:4
+$(document).ready(function () {
+    $('.category-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
         }
-    }
-})
-
+    })
+    
+    $('.testimonial-carousel').owlCarousel({
+        loop:true,
+        margin: 10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:4
+            }
+        }
+    })
+    $('.blog-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },
+            1000:{
+                items:3
+            }
+        }
+    });
+    new WOW().init();
+    
+});
