c0e7a08e909abf2d3854c47f9de8c1f63d4f6f90
Aytaj Nurullazada <aytacig@code.edu.az>
2021-02-09 22:49:53 +0400

    modal updated, scroll back to top added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,15 @@
 $(document).ready(function () {
+    $(window).scroll(function (e) {
+        if ($(this).scrollTop() > 84) {
+            $("header nav").addClass('header-fixed');
+            $("#back2Top").fadeIn();
+        } else {
+            $("header nav").removeClass('header-fixed');
+            $("#back2Top").fadeOut();
+        }
+    });
+
+    
     $('.category-carousel').owlCarousel({
         loop:true,
         margin:10,
@@ -50,4 +61,10 @@ $(document).ready(function () {
     });
     new WOW().init();
     
+    $("#back2Top").click(function () {
+        $("html, body").animate({
+            scrollTop: 0
+        }, 'slow');
+    });
 });
+
